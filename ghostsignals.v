2ee4a43fb14e7c62e9961645e0426314d5ba2638
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2023-09-14 19:59:01 +0200

    path_not_infinite

--- ghostsignals.v
+++ ghostsignals.v
@@ -1,5 +1,46 @@
-Require Import Init.Wf Relations.Relation_Operators Wellfounded.Lexicographic_Product List Classical ClassicalDescription ClassicalEpsilon.
+Require Import Init.Wf Relations.Relations Wellfounded.Lexicographic_Product List Classical ClassicalDescription ClassicalEpsilon.
 Import ListNotations.
+Require Import Lia.
+Require Import Nat.
+
+Lemma slexprod_trans {A B} (R1: A -> A -> Prop) (R2: B -> B -> Prop):
+  transitive _ R1 ->
+  transitive _ R2 ->
+  transitive _ (slexprod _ _ R1 R2).
+Proof.
+  intros.
+  intros [x1 x2] [y1 y2] [z1 z2] Hxy Hyz.
+  inversion Hxy; subst; inversion Hyz; subst.
+  + left.
+    apply (H _ _ _ H2 H3).
+  + left.
+    assumption.
+  + left.
+    assumption.
+  + right.
+    apply (H0 _ _ _ H2 H3).
+Qed.
+
+Lemma clos_refl_trans_r {A} R (x y z: A):
+  transitive _ R ->
+  R x y -> clos_refl _ R y z -> R x z.
+Proof.
+  intros.
+  inversion H1; subst.
+  - apply (H _ _ _ H0 H2).
+  - assumption.
+Qed.
+
+Lemma clos_refl_transitive {A} R:
+  transitive A R ->
+  transitive A (clos_refl _ R).
+Proof.
+  intros.
+  intros x y z Hxy Hyz.
+  inversion Hxy; subst; inversion Hyz; subst; try assumption.
+  left.
+  apply (H _ _ _ H0 H1).
+Qed.
 
 Definition If{T}(P: Prop)(t1 t2: T) := if excluded_middle_informative P then t1 else t2.
 
@@ -13,6 +54,7 @@ Parameter Binsert: forall {T}, T -> bag T -> bag T.
 Parameter Bremove1: forall {T}, T -> bag T -> bag T.
 Parameter Bflatmap: forall {A B}, (A -> bag B) -> bag A -> bag B.
 Parameter Blt: forall {T}, (T -> T -> Prop) -> bag T -> bag T -> Prop.
+Axiom Blt_trans: forall {T} (Tlt: T -> T -> Prop), transitive _ Tlt -> transitive _ (Blt Tlt).
 Axiom Blt_wf: forall {T} (Tlt: T -> T -> Prop), well_founded Tlt -> well_founded (Blt Tlt).
 
 Fixpoint Btimes{T}(n: nat)(e: T): bag T :=
@@ -27,11 +69,13 @@ Axiom level_lt_wf: well_founded level_lt.
 
 Parameter degree: Type.
 Parameter degree_lt: degree -> degree -> Prop.
+Axiom degree_lt_trans: transitive _ degree_lt.
 Axiom degree_lt_wf: well_founded degree_lt.
 
 Parameter cmd_size: Type.
 Parameter size_zero: cmd_size.
 Parameter size_lt: cmd_size -> cmd_size -> Prop.
+Axiom size_lt_trans: transitive _ size_lt.
 Axiom size_lt_wf: well_founded size_lt.
 
 Inductive phasecomp := Forker | Forkee.
@@ -162,13 +206,17 @@ Axiom finished_no_obs:
   configs i = Config st (Ts1 ++ [ThreadState size_zero ph obs] ++ Ts2) ->
   obs = Bempty.
 (* Fairness: If at a config i, some thread has not finished, then it takes a step at some index j >= i. *)
-Axiom fair:
-  forall i st Ts1 size ph obs Ts2,
+Inductive thread_alive i: nat -> Prop :=
+  thread_alive_intro st Ts1 size ph obs Ts2:
   configs i = Config st (Ts1 ++ [ThreadState size ph obs] ++ Ts2) ->
   size <> size_zero ->
+  thread_alive i (length Ts1).
+Axiom fair:
+  forall i t,
+  thread_alive i t ->
   exists j,
   i <= j /\
-  step_threads j = length Ts1.
+  step_threads j = t.
 
 Parameter CPs0: bag (thread_phase * degree).
 Parameter main_size0: cmd_size.
@@ -217,6 +265,14 @@ Definition forks_at(i: step_index)(forker: thread)(forkee: thread) :=
   labels i = Fork forkee_obs /\
   length Ts = forkee.
 
+Lemma forks_at_step_threads i t t':
+  forks_at i t t' -> step_threads i = t.
+Proof.
+  intros.
+  destruct H as [? [? [? ?]]].
+  tauto.
+Qed.
+
 Section Path.
 
   Variable p: cfg_index -> thread.
@@ -227,12 +283,47 @@ Section Path.
 
   Hypothesis His_path: is_path.
   
+  Lemma path_fork_step i j:
+    i <= j -> p j <> p i ->
+    exists k, i <= k /\ p k = p i /\ step_threads k = p k.
+  Proof.
+    induction j; intros.
+    - assert (i = 0). { lia. }
+      subst.
+      elim H0.
+      reflexivity.
+    - assert (i <= j). {
+        destruct (classic (i = S j)).
+        - subst. lia.
+        - lia.
+      }
+      destruct (classic (p j = p i)).
+      + destruct His_path.
+        destruct (H4 j).
+        * congruence.
+        * apply forks_at_step_threads in H5.
+          exists j. tauto.
+      + apply IHj; tauto.
+  Qed.
+  
+  Definition path_is_alive j := thread_alive j (p j).
+  
+  Lemma path_next_step i: path_is_alive i -> exists j, i <= j /\ step_threads j = p j.
+  Proof.
+    intro Halive.
+    apply fair in Halive.
+    destruct Halive as [j [Hij Hjpi]].
+    destruct (classic (p j = p i)).
+    - rewrite <- H in Hjpi.
+      exists j. tauto.
+    - apply path_fork_step in H.
+      + destruct H as [k [Hk1 [Hk2 Hk3]]].
+        exists k. tauto.
+      + assumption.
+  Qed.
+  
   Definition path_is_infinite :=
-    forall j,
-    exists st Ts1 size ph obs Ts2,
-    configs j = Config st (Ts1 ++ [ThreadState size ph obs] ++ Ts2) /\
-    length Ts1 = p j /\
-    size <> size_zero.
+    forall j, path_is_alive j.
   
   Hypothesis Hinf: path_is_infinite.
   
@@ -278,16 +369,87 @@ Section Path.
     let (size, ph, obs) := nth (p i) Ts (ThreadState size_zero [] Bempty) in
     (Bplus filtered_CPs filtered_WPs, size).
   
+  Definition fuel_lt := slexprod _ _ (Blt degree_lt) size_lt.
+  Definition fuel_le := clos_refl _ fuel_lt.
+  
   Lemma path_fuel_decreases_at_path_step(i: step_index):
     i0 <= i ->
     step_threads i = p i ->
-    slexprod _ _ (Blt degree_lt) size_lt (path_fuel_at i) (path_fuel_at (S i)).
+    fuel_lt (path_fuel_at (S i)) (path_fuel_at i).
   Admitted.
   
   Lemma path_fuel_does_not_increase_at_non_path_step(i: step_index):
     i0 <= i ->
     step_threads i <> p i ->
-    clos_refl _ (slexprod _ _ (Blt degree_lt) size_lt) (path_fuel_at i) (path_fuel_at (S i)).
+    fuel_le (path_fuel_at (S i)) (path_fuel_at i).
   Admitted.
   
+  Lemma fuel_lt_transitive: transitive _ fuel_lt.
+  Proof.
+    apply slexprod_trans.
+    * apply Blt_trans. apply degree_lt_trans.
+    * apply size_lt_trans.
+  Qed.
   
+  Lemma fuel_le_transitive: transitive _ fuel_le.
+  Proof.
+    apply (clos_refl_transitive _ fuel_lt_transitive).
+  Qed.
+  
+  Lemma fuel_lt_le x y z: fuel_lt x y -> fuel_le y z -> fuel_lt x z.
+  Proof.
+    apply clos_refl_trans_r.
+    apply fuel_lt_transitive.
+  Qed.
+  
+  Lemma path_fuel_mono i j:
+    i0 <= i ->
+    i <= j ->
+    fuel_le (path_fuel_at j) (path_fuel_at i).
+  Proof.
+    intro.
+    revert j.
+    intro j.
+    induction j; intros.
+    - assert (i = 0). { lia. }
+      subst.
+      right.
+    - destruct (classic (i = S j)).
+      + subst.
+        right.
+      + apply (fuel_le_transitive _ (path_fuel_at j) _).
+        * destruct (classic (step_threads j = p j)).
+          -- left.
+             apply path_fuel_decreases_at_path_step; [lia | assumption].
+          -- apply path_fuel_does_not_increase_at_non_path_step; [lia | assumption].
+        * apply IHj; lia.
+  Qed.
+  
+  Lemma path_fuel_wf: well_founded (slexprod _ _ (Blt degree_lt) size_lt).
+  Proof.
+    apply wf_slexprod.
+    - apply Blt_wf.
+      apply degree_lt_wf.
+    - apply size_lt_wf.
+  Qed.
+  
+  Lemma path_not_infinite: False.
+  Proof.
+    assert (forall d, forall i, i0 <= i -> d = path_fuel_at i -> False). {
+      intro d.
+      apply (well_founded_ind path_fuel_wf) with (a:=d).
+      intros.
+      clear d.
+      subst.
+      destruct (path_next_step i (Hinf i)) as [j [Hj1 Hj2]].
+      apply (H (path_fuel_at (S j))) with (i:=S j).
+      - apply fuel_lt_le with (y:=path_fuel_at j).
+        + apply (path_fuel_decreases_at_path_step j).
+          * lia.
+          * assumption.
+        + apply path_fuel_mono; lia.
+      - lia.
+      - reflexivity.
+    }
+    apply (H (path_fuel_at i0) i0); [ lia | reflexivity ].
+  Qed.
